ce6053a63dc3699570cb02f644165fc054982530
Joose Ahonen <t0ahjo00@students.oamk.fi>
2021-11-18 18:32:26 +0200

    Lisätty 1 javatiedosto ja xml

---
+++ app/src/main/java/reservation/system/Appointment.java
@@ -0,0 +1,21 @@
+package reservation.system;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Appointment extends AppCompatActivity implements View.OnClickListener{
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_appointment);
+        }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
+
--- app/src/main/java/reservation/system/ProfileActivity.java
+++ app/src/main/java/reservation/system/ProfileActivity.java
@@ -1,14 +1,47 @@
 package reservation.system;
 
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class ProfileActivity extends AppCompatActivity {
+public class ProfileActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private Button appointment;
+    private Button profile;
+    private Button reservations;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        appointment = (Button) findViewById(R.id.appointmentID);
+        appointment.setOnClickListener(this);
+
+        profile = (Button) findViewById(R.id.profileID);
+        profile.setOnClickListener(this);
+
+        reservations = (Button) findViewById(R.id.reservationsID);
+        reservations.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.profileID:
+
+                break;
+            case R.id.appointmentID:
+                startActivity(new Intent(this,Appointment.class));
+                break;
+            case R.id.reservationsID:
+
+                break;
+        }
     }
 }
