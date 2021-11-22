9539fa6804aa19211065adb73cc41cee0eb3b161
miikkam <miikkamoilanen@gmail.com>
2021-11-22 23:01:29 +0200

    uloskirjautuminen
    uloskirjautuminen lisätty profileactivityyn

--- app/src/main/java/reservation/system/ProfileActivity.java
+++ app/src/main/java/reservation/system/ProfileActivity.java
@@ -8,11 +8,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class ProfileActivity extends AppCompatActivity implements View.OnClickListener{
 
     private Button appointment;
     private Button profile;
     private Button reservations;
+    private Button logout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +30,15 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
 
         reservations = (Button) findViewById(R.id.reservationsID);
         reservations.setOnClickListener(this);
+
+        logout = (Button) findViewById(R.id.signOut);
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(ProfileActivity.this, MainActivity.class));
+            }
+        });
     }
 
     @Override
