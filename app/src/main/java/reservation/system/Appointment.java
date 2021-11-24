dd0f05ab85f4084ed8c11c25e226cfc071cfc09a
Eliel241 <eliel.latvala@gmail.com>
2021-11-24 17:49:51 +0200

    Pöytä varauksia testailtu

--- app/src/main/java/reservation/system/Appointment.java
+++ app/src/main/java/reservation/system/Appointment.java
@@ -6,8 +6,12 @@ import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class Appointment extends AppCompatActivity implements View.OnClickListener{
         private Button btn1,btn2;
@@ -34,6 +38,18 @@ public class Appointment extends AppCompatActivity implements View.OnClickListen
         }
         else{
             btn1.setSelected(true);
+        }
+    }
+    public void Reserve(View view){
+        if(btn1.isSelected())
+        {
+            FirebaseDatabase.getInstance().getReference("Tables").child(FirebaseAuth.getInstance()
+                    .getCurrentUser().getUid()).setValue(btn1.getText());
+            Toast.makeText(Appointment.this, "Table 1 Reserved!", Toast.LENGTH_LONG).show();
+        }
+        else
+        {
+            Toast.makeText(Appointment.this, "Select a table", Toast.LENGTH_LONG).show();
         }
     }
 }
