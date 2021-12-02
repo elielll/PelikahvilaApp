18be20b7004bf0288ef073c464dbbbcd7add1dd4
Eliel241 <eliel.latvala@gmail.com>
2021-12-02 15:57:43 +0200

    Varausaika ja päivä näkyy tietokannassa

--- app/src/main/java/reservation/system/Appointment.java
+++ app/src/main/java/reservation/system/Appointment.java
@@ -14,12 +14,13 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.Calendar;
 
 public class Appointment extends AppCompatActivity implements View.OnClickListener{
-        private Button btn1,btn2,btn3;
+        private Button btn1,btn2,btn3,btn4,btn5,btn6,btn7,btn8,btn9,btn10,btn11,btn12;
         EditText etDate;
         EditText etTime;
 
@@ -37,6 +38,34 @@ public class Appointment extends AppCompatActivity implements View.OnClickListen
 
             btn3 = (Button) findViewById(R.id.button28);
             btn3.setOnClickListener(this);
+
+            btn4 = (Button) findViewById(R.id.button29);
+            btn4.setOnClickListener(this);
+
+            btn5 = (Button) findViewById(R.id.button30);
+            btn5.setOnClickListener(this);
+
+            btn6 = (Button) findViewById(R.id.button31);
+            btn6.setOnClickListener(this);
+
+            btn7 = (Button) findViewById(R.id.button32);
+            btn7.setOnClickListener(this);
+
+            btn8 = (Button) findViewById(R.id.button33);
+            btn8.setOnClickListener(this);
+
+            btn9 = (Button) findViewById(R.id.button34);
+            btn9.setOnClickListener(this);
+
+            btn10 = (Button) findViewById(R.id.button35);
+            btn10.setOnClickListener(this);
+
+            btn11 = (Button) findViewById(R.id.button36);
+            btn11.setOnClickListener(this);
+
+            btn12 = (Button) findViewById(R.id.button37);
+            btn12.setOnClickListener(this);
+
 
             etDate = findViewById(R.id.et_date);
             etTime = findViewById(R.id.et_time);
@@ -84,6 +113,7 @@ public class Appointment extends AppCompatActivity implements View.OnClickListen
 
         @Override
         public void onClick(View v) {
+            //jos keksii jonku paremman ja lyhyemmän keinon tehä tää ni voi tehä, buttoniin 8 asti tehty
 
             switch (v.getId()){
                 case R.id.button26:
@@ -116,19 +146,153 @@ public class Appointment extends AppCompatActivity implements View.OnClickListen
                     }
                     break;
 
+                case R.id.button29:
+                    if(btn4.isSelected()) {
+
+                        btn4.setSelected(false);
+                    }
+                    else{
+                        btn4.setSelected(true);
+                    }
+                    break;
+
+                case R.id.button30:
+                    if(btn5.isSelected()) {
+
+                        btn5.setSelected(false);
+                    }
+                    else{
+                        btn5.setSelected(true);
+                    }
+                    break;
+
+                case R.id.button31:
+                    if(btn6.isSelected()) {
+
+                        btn6.setSelected(false);
+                    }
+                    else{
+                        btn6.setSelected(true);
+                    }
+                    break;
+
+                case R.id.button32:
+                    if(btn7.isSelected()) {
+
+                        btn7.setSelected(false);
+                    }
+                    else{
+                        btn7.setSelected(true);
+                    }
+                    break;
+
+                case R.id.button33:
+                    if(btn8.isSelected()) {
+
+                        btn8.setSelected(false);
+                    }
+                    else{
+                        btn8.setSelected(true);
+                    }
+                    break;
+
+
             }
         }
-    public void Reserve(View view){
+        // varaus nappia painettaessa katsotaan, että mikä pvm, aika ja pöytä on valittu ja lähetetään
+        //firebaseen
+    public void Reserve(View v){
+        //haetaan valitut aika ja päivä muuttujiin
+        String time = etTime.getText().toString().trim();
+        String date = etDate.getText().toString().trim();
+
+        //tähä pitäs keksiä joku juttu että tuo pöydän numero vaihtuu sen mukaan mikä on valittu
+        // nyt se laittaa aina tuon pöydän 1 iha sama minkä valitsee.
+        //Tossa kommentoituna tuo switch jolla se vois ehkä toimia
+        String table = btn1.getText().toString().trim();
+        /*switch (v.getId())
+        {
+            case R.id.button26:
+                 table = btn1.getText().toString().trim();
+                 break;
+
+            case R.id.button27:
+                 table = btn2.getText().toString().trim();
+                 break;
+
+            case R.id.button28:
+                table = btn3.getText().toString().trim();
+
+        }
+
+         */
+        //FirebaseUser name = FirebaseAuth.getInstance().getCurrentUser();
+
+        User user = new User(date, time, table);
+        // tässäkin jos keksii jonku paremman keinon tehä tuo niin hyvä ku aika pitkä koodin pätkä
+        // ja buttoniin 8 asti tehty
         if(btn1.isSelected())
         {
             FirebaseDatabase.getInstance().getReference("Tables").child(FirebaseAuth.getInstance()
-                    .getCurrentUser().getUid()).setValue(btn1.getText());
+                    .getCurrentUser().getUid()).setValue(user);
             Toast.makeText(Appointment.this, "Table 1 Reserved!", Toast.LENGTH_LONG).show();
+            btn1.setClickable(false); // asetetaan buttoni falseksi ettei sitä voi enää muutta ku varaus tehty
+        }
+
+        if(btn2.isSelected())
+        {
+            FirebaseDatabase.getInstance().getReference("Tables").child(FirebaseAuth.getInstance()
+                    .getCurrentUser().getUid()).setValue(user);
+            Toast.makeText(Appointment.this, "Table 2 Reserved!", Toast.LENGTH_LONG).show();
+            btn2.setClickable(false);
+        }
+        if(btn3.isSelected())
+        {
+            FirebaseDatabase.getInstance().getReference("Tables").child(FirebaseAuth.getInstance()
+                    .getCurrentUser().getUid()).setValue(user);
+            Toast.makeText(Appointment.this, "Table 3 Reserved!", Toast.LENGTH_LONG).show();
+            btn3.setClickable(false);
+        }
+        if(btn4.isSelected())
+        {
+            FirebaseDatabase.getInstance().getReference("Tables").child(FirebaseAuth.getInstance()
+                    .getCurrentUser().getUid()).setValue(user);
+            Toast.makeText(Appointment.this, "Table 4 Reserved!", Toast.LENGTH_LONG).show();
+            btn4.setClickable(false);
+        }
+        if(btn5.isSelected())
+        {
+            FirebaseDatabase.getInstance().getReference("Tables").child(FirebaseAuth.getInstance()
+                    .getCurrentUser().getUid()).setValue(user);
+            Toast.makeText(Appointment.this, "Table 5 Reserved!", Toast.LENGTH_LONG).show();
+            btn5.setClickable(false);
+        }
+        if(btn6.isSelected())
+        {
+            FirebaseDatabase.getInstance().getReference("Tables").child(FirebaseAuth.getInstance()
+                    .getCurrentUser().getUid()).setValue(user);
+            Toast.makeText(Appointment.this, "Table 6 Reserved!", Toast.LENGTH_LONG).show();
+            btn6.setClickable(false);
+        }
+        if(btn7.isSelected())
+        {
+            FirebaseDatabase.getInstance().getReference("Tables").child(FirebaseAuth.getInstance()
+                    .getCurrentUser().getUid()).setValue(user);
+            Toast.makeText(Appointment.this, "Table 7 Reserved!", Toast.LENGTH_LONG).show();
+            btn7.setClickable(false);
+        }
+        if(btn8.isSelected())
+        {
+            FirebaseDatabase.getInstance().getReference("Tables").child(FirebaseAuth.getInstance()
+                    .getCurrentUser().getUid()).setValue(user);
+            Toast.makeText(Appointment.this, "Table 8 Reserved!", Toast.LENGTH_LONG).show();
+            btn8.setClickable(false);
         }
         else
         {
             Toast.makeText(Appointment.this, "Select a table", Toast.LENGTH_LONG).show();
         }
+
     }
 }
 
--- app/src/main/java/reservation/system/User.java
+++ app/src/main/java/reservation/system/User.java
@@ -1,11 +1,22 @@
 package reservation.system;
 
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseUser;
+
 public class User {
     public String fullName, email;
+    public String date, time;
+    public String table;
+    //public FirebaseUser name;
 
-    public User()
+
+    //konstruktori varaustietojen tallettamista varten firebaseen
+    public User(String date,String time, String table)
     {
-
+        this.date=date;
+        this.time=time;
+        this.table=table;
     }
 
     public User(String fullName, String email)
