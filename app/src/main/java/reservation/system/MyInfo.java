5e3f1f3e53d20f097dceb7bb4825d54911bac9c8
Joose Ahonen <t0ahjo00@students.oamk.fi>
2021-12-08 16:51:48 +0200

    jopasjotaki

---
+++ app/src/main/java/reservation/system/MyInfo.java
@@ -0,0 +1,65 @@
+package reservation.system;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class MyInfo extends AppCompatActivity {
+
+
+    private Button back;
+
+    private FirebaseUser user;
+    private DatabaseReference reference;
+    private String userID;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_info);
+
+
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        reference = FirebaseDatabase.getInstance().getReference("Users");
+        userID = user.getUid();
+
+        final TextView emailTextView = (TextView) findViewById(R.id.emailAddress);
+        final TextView fullNameTextView = (TextView) findViewById(R.id.fullName);
+
+        reference.child(userID).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                User userProfile = snapshot.getValue(User.class);
+
+                if(userProfile != null) {
+                    String email = userProfile.email;
+                    String fullName = userProfile.fullName;
+
+                    emailTextView.setText(email);
+                    fullNameTextView.setText(fullName);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(MyInfo.this, "Something went wrong!", Toast.LENGTH_LONG).show();
+            }
+        });
+
+    }
+
+}
